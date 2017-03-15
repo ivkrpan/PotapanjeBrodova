cc65c0e0ac558226cc520619f0f597cd78e8696c
ivkrpan <ivkrpan@vsite.hr>
2017-03-15 21:26:05 +0100

    gotovo

---
+++ PotapanjeBrodova/Test/TestMreze.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class TestMreze
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
