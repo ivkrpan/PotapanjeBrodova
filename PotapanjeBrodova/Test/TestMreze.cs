52de0baa1b75a37a3931a58cc12b8605c435104f
ivkrpan <ivkrpan@vsite.hr>
2017-03-22 20:47:13 +0100

    Prva verzija mreze

--- PotapanjeBrodova/Test/TestMreze.cs
+++ PotapanjeBrodova/Test/TestMreze.cs
@@ -1,14 +1,97 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using potapanjeBrodova;
+using System.Linq;
 namespace Test
 {
     [TestClass]
     public class TestMreze
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Mreza_DajSlobodnaPoljaVracaSvaPoljaZaPocetnuMrezu()
         {
+            Mreza m = new Mreza(5, 5);
+            Assert.AreEqual(25, m.DajSlobodnaPolja().Count());
+
+        }
+        [TestMethod]
+        public void Mreza_DajSlobodnaPoljaVraca24PoljaZa5x5NakonUklonjenogPoljaZadanogRedomIStupcem()
+        {
+            Mreza m = new Mreza(5, 5);
+            Assert.AreEqual(25, m.DajSlobodnaPolja().Count());
+            m.UkloniPolje(1,1);
+            Assert.AreEqual(24, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1))); 
+
+        }
+
+        [TestMethod]
+        public void Mreza_DajSlobodnaPoljaVraca24PoljaZa5x5NakonUklonjenogPolja()
+        {
+            Mreza m = new Mreza(5, 5);
+            Assert.AreEqual(25, m.DajSlobodnaPolja().Count());
+            m.UkloniPolje(new Polje(1, 1));
+            Assert.AreEqual(24, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+
+        }
+
+        [TestMethod]
+        public void Mreza_DajSlobodnaPoljaVraca23PoljaZa5x5NakonDvaUklonjenaPolja()
+        {
+            Mreza m = new Mreza(5, 5);
+            m.UkloniPolje(1, 1);
+            m.UkloniPolje(4, 4);
+            Assert.AreEqual(23, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(4, 4)));
+        }
+        [TestMethod]
+
+        public void Mreza_UkloniPoljeBacaIznimkuZaNepostojeciRedak()
+        {
+
+            try
+            {
+                Mreza m = new Mreza(5, 5);
+                m.UkloniPolje(6, 1);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+       
+    
+        }
+
+        [TestMethod]
+
+        public void Mreza_UkloniPoljeBacaIznimkuZaNepostojeciStupac()
+        {
+
+            try
+            {
+                Mreza m = new Mreza(5, 5);
+                m.UkloniPolje(1, 6);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+
+
         }
     }
 }
--- PotapanjeBrodova/potapanjeBrodova/Mreza.cs
+++ PotapanjeBrodova/potapanjeBrodova/Mreza.cs
@@ -8,6 +8,50 @@ namespace potapanjeBrodova
 {
     public class Mreza
     {
+        private int stupaca;
+        private int redaka;
 
+        public Mreza(int redaka, int stupaca)
+        {
+            this.redaka = redaka;
+            this.stupaca = stupaca;
+            polja = new Polje[redaka, stupaca];
+            for (int r =0; r< redaka; r++) {
+                for (int s = 0; s < stupaca; s++)
+                {
+                    polja[r, s] = new Polje(r, s);
+                }
+            }
+        }
+        public IEnumerable<Polje> DajSlobodnaPolja()
+        {
+            List<Polje> p = new List<Polje>();
+            for (int r = 0; r < redaka; r++)
+            {
+                for (int s = 0; s < stupaca; s++)
+                {
+
+                    if (polja[r,s] != null) {
+                        p.Add(polja[r, s]);
+
+                    }
+
+                }
+            }
+
+            return p;
+//            throw new NotImplementedException();
+        }
+        public void UkloniPolje(int redak, int stupca)
+        {
+            polja[redak, stupca] = null;
+ 
+
+        }
+        public void UkloniPolje(Polje p) {
+            polja[p.Redak, p.Stupac] = null;
+        }
+
+        private Polje[,] polja;
     }
 }
