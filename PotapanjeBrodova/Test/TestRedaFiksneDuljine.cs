43a0aff8fd82e395e3cc32096748a3102d2f611f
ivkrpan <ivkrpan@vsite.hr>
2017-03-29 20:48:01 +0200

    dodani test za red fiksne duljine i implementacija takvog reda

---
+++ PotapanjeBrodova/Test/TestRedaFiksneDuljine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using potapanjeBrodova;
+
+namespace Test
+{
+    [TestClass]
+    public class TestRedaFiksneDuljine
+    {
+        [TestMethod]
+        public void RedFIksneDuljine_DodavanjeJednogClanaPovecavaDuljinuReda()
+        {
+            RedFiksneDuljine<Polje> r = new RedFiksneDuljine<Polje>(3);
+            Assert.AreEqual(0, r.Count);
+            r.Enqueue(new Polje(0,0));
+            Assert.AreEqual(1, r.Count);
+        }
+
+        [TestMethod]
+        public void RedFIksneDuljine_DodavanjeCetvrtogClanaNePovecavaRedDuljine3()
+        {
+            RedFiksneDuljine<Polje> r = new RedFiksneDuljine<Polje>(3);
+   
+            r.Enqueue(new Polje(0, 0));
+            r.Enqueue(new Polje(1, 0));
+            r.Enqueue(new Polje(2, 0));
+            Assert.AreEqual(3, r.Count);
+            r.Enqueue(new Polje(3, 0));
+            Assert.AreEqual(3, r.Count);
+        }
+        [TestMethod]
+        public void RedFIksneDuljine_DodavanjeCetvrtogClanaIzbacujePrviClanIzRedaDuljin3()
+        {
+            RedFiksneDuljine<Polje> r = new RedFiksneDuljine<Polje>(3);
+
+            r.Enqueue(new Polje(0, 0));
+            r.Enqueue(new Polje(1, 0));
+            r.Enqueue(new Polje(2, 0));
+            Assert.IsTrue( r.Contains(new Polje(0,0)));
+            r.Enqueue(new Polje(3, 0));
+            Assert.IsFalse(r.Contains(new Polje(0, 0)));
+        }
+        [TestMethod]
+        public void RedFIksneDuljine_BrisanjeUklanjaSveClanoveIzReda()
+        {
+
+            RedFiksneDuljine<Polje> r = new RedFiksneDuljine<Polje>(3);
+
+            r.Enqueue(new Polje(0, 0));
+            r.Enqueue(new Polje(1, 0));
+            r.Enqueue(new Polje(2, 0));
+            r.Clear();
+            Assert.AreEqual(0, r.Count);
+        }
+    }
+}
---
+++ PotapanjeBrodova/potapanjeBrodova/RedFiksneDuljine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace potapanjeBrodova
+{
+    public class RedFiksneDuljine<T> : Queue<T>
+    {
+        public RedFiksneDuljine(int maksimalnaDuljina) {
+            this.maksimalnaDuljina = maksimalnaDuljina;
+
+        }
+
+        public new void Enqueue(T element) {
+            base.Enqueue(element);
+            while (base.Count > maksimalnaDuljina) {
+                Dequeue();
+            }
+        }
+        private int maksimalnaDuljina;
+    }
+}
