8de2c206b628b68e7e50291ccb6321fa4e64b9e5
ivkrpan <ivkrpan@vsite.hr>
2017-05-03 20:48:29 +0200

    Implementirano Topnistvo, Interface i Testovi!

---
+++ PotapanjeBrodova/Test/TestTopnistva.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using potapanjeBrodova;
+namespace Test
+{
+    [TestClass]
+    public class TestTopnistva
+    {
+        [TestMethod]
+        public void Topnistvo_NaPocetkuJeTaktikaNasumicna()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3 ,2 ,1 };
+            Topnistvo t = new Topnistvo(redaka,stupaca,duljineBrodova);
+            Assert.AreEqual(TaktikaGadjanja.Nasumicno,t.TaktikaGadjanja);
+        }
+
+        [TestMethod]
+        public void Topnistvo_NasumicnaTaknikaNakonPromasajaOstajeNasumicna()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Promašaj);
+            Assert.AreEqual(TaktikaGadjanja.Nasumicno, t.TaktikaGadjanja);
+        }
+
+        [TestMethod]
+        public void Topnistvo_NasumicnaTaknikaNakonPotapljanjaOstajeNasumicna()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Potopljen);
+            Assert.AreEqual(TaktikaGadjanja.Nasumicno, t.TaktikaGadjanja);
+        }
+
+
+        [TestMethod]
+        public void Topnistvo_NakonPrvogPogotkaTaktikaPrelaziUKruzno()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            Assert.AreEqual(TaktikaGadjanja.Kruzno, t.TaktikaGadjanja);
+        }
+
+        [TestMethod]
+        public void Topnistvo_KruznoGadjanjeNakonPromasajaOstajeKruzno()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Promašaj);
+            Assert.AreEqual(TaktikaGadjanja.Kruzno, t.TaktikaGadjanja);
+        }
+        [TestMethod]
+        public void Topnistvo_KruznoGadjanjeNakonPogotkaPrelaziULinijsko()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            Assert.AreEqual(TaktikaGadjanja.Linijsko, t.TaktikaGadjanja);
+        }
+        [TestMethod]
+        public void Topnistvo_KruznoGadjanjeNakonPotopaBrodaPrelaziUNasumicno()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Potopljen);
+            Assert.AreEqual(TaktikaGadjanja.Nasumicno, t.TaktikaGadjanja);
+        }
+        [TestMethod]
+        public void Topnistvo_LinijskoGadjanjeNakonPromasajaOstajeLinijsko()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Promašaj);
+            Assert.AreEqual(TaktikaGadjanja.Linijsko, t.TaktikaGadjanja);
+        }
+        [TestMethod]
+        public void Topnistvo_LinijskoGadjanjeNakonPotopaBrodaPostajeNasumicno()
+        {
+            int redaka = 5;
+            int stupaca = 5;
+            int[] duljineBrodova = new int[] { 3, 2, 1 };
+            Topnistvo t = new Topnistvo(redaka, stupaca, duljineBrodova);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Pogodak);
+            t.ObradiGadjanje(RezultatGadjanja.Potopljen);
+            Assert.AreEqual(TaktikaGadjanja.Nasumicno, t.TaktikaGadjanja);
+        }
+
+    }
+}
---
+++ PotapanjeBrodova/potapanjeBrodova/Topnistvo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace potapanjeBrodova
+{
+    public enum TaktikaGadjanja {
+        Nasumicno,
+        Kruzno,
+        Linijsko
+    }
+
+    public class Topnistvo
+    {
+        public Topnistvo(int redaka, int stupaca, IEnumerable<int> duljineBrodova) {
+            mreza = new Mreza(redaka, stupaca);
+            this.duljineBrodova = new List<int>(duljineBrodova);
+            TaktikaGadjanja = TaktikaGadjanja.Nasumicno;
+    }
+
+        public void ObradiGadjanje(RezultatGadjanja rezultat) {
+            if (rezultat == RezultatGadjanja.Promašaj)
+            {
+                return;
+            }
+
+            if (rezultat == RezultatGadjanja.Pogodak)
+            {
+                switch (TaktikaGadjanja) {
+
+                    case TaktikaGadjanja.Nasumicno:
+                        PromijeniTaktikuUKruzno();
+                        return;
+                    case TaktikaGadjanja.Kruzno:
+                        PromijeniTaktikuULinijsko();
+                        return;
+                    case TaktikaGadjanja.Linijsko:
+                        return;
+                    default:
+                        Debug.Assert(false);
+                        break;
+                        
+                }
+
+            }
+            Debug.Assert(rezultat == RezultatGadjanja.Potopljen);
+            PromijeniTaktikuUNasumicno();
+            
+        }
+
+       private  void PromijeniTaktikuUKruzno() {
+            TaktikaGadjanja = TaktikaGadjanja.Kruzno;
+        }
+       private  void PromijeniTaktikuULinijsko() {
+            TaktikaGadjanja = TaktikaGadjanja.Linijsko;
+        }
+       private  void PromijeniTaktikuUNasumicno() {
+            TaktikaGadjanja = TaktikaGadjanja.Nasumicno;
+        }
+
+
+        public TaktikaGadjanja TaktikaGadjanja { get; private set; }
+
+      private  Mreza mreza;
+      private  List<int> duljineBrodova;
+     
+    }
+}
